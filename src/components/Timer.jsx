9fe3b2f802f9bc4326422713c17bc10ccdfe75bf
ericstober <erictstober@gmail.com>
2025-11-11 10:24:47 -0600

    persist time to local storage

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,18 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 
 const Timer = () => {
   const timerRef = useRef(null);
-  const [time, setTime] = useState(0);
+
+  const [time, setTime] = useState(() => {
+    return Number(localStorage.getItem("time") || 0);
+  });
   const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("time", time);
+  }, [time]);
 
   const toggleTimer = () => {
     if (isRunning) {
@@ -27,6 +34,7 @@ const Timer = () => {
     setIsRunning(false);
     setTime(0);
     timerRef.current = null;
+    localStorage.removeItem("time");
   };
 
   return (
