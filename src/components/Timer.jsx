bb52c08ae3d00424217fcd4665e2b424d59d2f0c
ericstober <erictstober@gmail.com>
2025-11-11 09:56:11 -0600

    modularize code by extracting code to components

---
+++ src/components/Timer.jsx
@@ -0,0 +1,40 @@
+import { useState, useRef } from "react";
+import TimerDisplay from "./TimerDisplay";
+import TimerControls from "./TimerControls";
+
+const Timer = () => {
+  const timerRef = useRef(null);
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      // Clear interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // Start the timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null;
+  };
+
+  return (
+    <div>
+      <TimerDisplay time={time} />
+      <TimerControls isRunning={isRunning} onToggle={toggleTimer} onReset={resetTimer} />
+    </div>
+  );
+};
+
+export default Timer;
---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,5 @@
+const TimerDisplay = ({ time }) => {
+  return <h2 className='text-4xl font-semibold mt-4'>&#x23F3; Timer: {time}</h2>;
+};
+
+export default TimerDisplay;
